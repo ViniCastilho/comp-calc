c2924fe1e014394ede4d0cb5bff85da4c3b25e0a
Vinícius Andrei Parra Castilho <vinigamer83@gmail.com>
2025-02-20 15:21:46 -0300

    Progress

--- main.js
+++ main.js
@@ -3,3 +3,17 @@ let btnFile = document.querySelector('#btn-file');
 let txtResult = document.querySelector('#txt-result');
 
 let tokens = [];
+
+function calcLoad(event) {
+	let stream = event.target.result;
+	let pattern = /(\x20|\x09|\x0A|\x0D)/g;
+	stream = stream.replaceAll(pattern, '');
+	console.log(stream);
+}
+
+btnCalc.onclick = () => {
+	let upload = btnFile.files[0];
+	let reader = new FileReader();
+	reader.readAsText(upload, 'UTF-8');
+	reader.onload = calcLoad;
+}

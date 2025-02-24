dd889aaef9488200321beaf0faa2bdb3ae29883e
ViniCastilho <vinigamer83@gmail.com>
2025-02-24 15:53:40 -0300

    Progress

--- main.js
+++ main.js
@@ -2,13 +2,40 @@ let btnCalc = document.querySelector('#btn-calc');
 let btnFile = document.querySelector('#btn-file');
 let txtResult = document.querySelector('#txt-result');
 
-let tokens = [];
+let mapOperatorPriority = {
+	'+': 0,
+	'-': 0,
+	'*': 1,
+	'/': 1,
+	'^': 2,
+};
+
+let mapValidSymbols = {
+	'.': true,
+};
+for (let i = 0; i < 10; i++) {
+	mapValidSymbols[toString(i)] = true;
+}
+for (let [k, v] of Object.entries(mapOperatorPriority)) {
+	mapValidSymbols[k] = true;
+}
+
+function runLexer(stream) {
+	let splitPattern = /(\+|\-|\*|\/|\^)/;
+	let tokens = stream.split(splitPattern);
+	console.log(tokens);
+}
 
 function calcLoad(event) {
 	let stream = event.target.result;
 	let pattern = /(\x20|\x09|\x0A|\x0D)/g;
 	stream = stream.replaceAll(pattern, '');
 	console.log(stream);
+	runLexer(stream);
+}
+
+function calcError(event) {
+	console.log('Failed to load input file');
 }
 
 btnCalc.onclick = () => {
@@ -16,4 +43,5 @@ btnCalc.onclick = () => {
 	let reader = new FileReader();
 	reader.readAsText(upload, 'UTF-8');
 	reader.onload = calcLoad;
+	reader.onerror = calcError;
 }
